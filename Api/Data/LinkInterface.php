6ecfe4d2beb372a4e2f95531997850c6501c098c
Weverson Cachinsky <weversoncachinsky@gmail.com>
2021-06-07 17:15:24 -0300

    Use strict types

--- Api/Data/LinkInterface.php
+++ Api/Data/LinkInterface.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Weverson83\AddByLink\Api\Data;
 
--- Api/Data/LinkProductInterface.php
+++ Api/Data/LinkProductInterface.php
@@ -1,8 +1,7 @@
 <?php
-
+declare(strict_types=1);
 
 namespace Weverson83\AddByLink\Api\Data;
-
 
 interface LinkProductInterface
 {
