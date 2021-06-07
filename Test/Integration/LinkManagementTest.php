36daa6c6e2646588c52571478b4989faa804ab18
Weverson Cachinsky <weversoncachinsky@gmail.com>
2021-06-07 17:15:40 -0300

    Setup module integration tests

---
+++ Test/Integration/LinkManagementTest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Weverson83\AddByLink\Test\Integration;
+
+class LinkManagementTest extends \PHPUnit\Framework\TestCase
+{
+    public function testNothing()
+    {
+        $this->fail('=)');
+    }
+}
