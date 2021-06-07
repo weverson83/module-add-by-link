f331723b793cc6fb1e9afe7840a095e3675fb0ec
Weverson Cachinsky <weversoncachinsky@gmail.com>
2021-06-07 16:46:08 -0300

    Create db tables and interfaces

---
+++ Api/Data/LinkInterface.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Weverson83\AddByLink\Api\Data;
+
+interface LinkInterface
+{
+    /**#@+
+     * Constants for keys of data array.
+     */
+    const ID = 'entity_id';
+    const TOKEN = 'token';
+    const TOKEN_CREATED_AT = 'token_created_at';
+    const EXPIRATION_PERIOD = 'expiration_period';
+    /**#@-*/
+
+    /**
+     * @return int
+     */
+    public function getId(): int;
+
+    /**
+     * @param int $entityId
+     * @return \Weverson83\AddByLink\Api\Data\LinkInterface
+     */
+    public function setId(int $entityId): LinkInterface;
+
+    /**
+     * Retrieve token hash string
+     *
+     * @return string
+     */
+    public function getToken(): string;
+
+    /**
+     * Set token hash string
+     *
+     * @param string $token
+     * @return \Weverson83\AddByLink\Api\Data\LinkInterface
+     */
+    public function setToken(string $token): LinkInterface;
+
+    /**
+     * Retrieve token's creation timestamp
+     *
+     * @return string
+     */
+    public function getTokenCreatedAt(): string;
+
+    /**
+     * Set token's creation timestamp
+     *
+     * @param string $tokenCreatedAt
+     * @return \Weverson83\AddByLink\Api\Data\LinkInterface
+     */
+    public function setTokenCreatedAt(string $tokenCreatedAt): LinkInterface;
+
+    /**
+     * Retrieve expiration period in hours
+     *
+     * @return int
+     */
+    public function getExpirationPeriod(): int;
+
+    /**
+     * Set expiration period in hours
+     *
+     * @param int $expirationPeriod
+     * @return \Weverson83\AddByLink\Api\Data\LinkInterface
+     */
+    public function setExpirationPeriod(int $expirationPeriod): LinkInterface;
+}
---
+++ Api/Data/LinkProductInterface.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace Weverson83\AddByLink\Api\Data;
+
+
+interface LinkProductInterface
+{
+    /**#@+
+     * Constants for keys of data array.
+     */
+    const PRODUCT_ID = 'product_id';
+    const LINK_ID = 'link_id';
+    /**#@-*/
+
+    /**
+     * Retrieve product's ID
+     *
+     * @return int
+     */
+    public function getProductId(): int;
+
+    /**
+     * Set product ID
+     *
+     * @param int $productId
+     * @return \Weverson83\AddByLink\Api\Data\LinkProductInterface
+     */
+    public function setProductId(int $productId): LinkProductInterface;
+
+    /**
+     * Retrieve link ID
+     *
+     * @return int
+     */
+    public function getLinkId(): int;
+
+    /**
+     * Set link ID
+     *
+     * @param int $linkId
+     * @return \Weverson83\AddByLink\Api\Data\LinkProductInterface
+     */
+    public function setLinkId(int $linkId): LinkProductInterface;
+}
