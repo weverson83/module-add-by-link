f0b990eba83313b8422e0509cb6252ceadcdc4c4
Weverson Cachinsky <weversoncachinsky@gmail.com>
2021-06-08 12:29:30 -0300

    Remove unecessary classes

--- Model/Link/ReadHandler.php
+++ Model/Link/ReadHandler.php
@@ -4,15 +4,36 @@ declare(strict_types=1);
 namespace Weverson83\AddByLink\Model\Link;
 
 use Magento\Framework\EntityManager\Operation\ExtensionInterface;
+use Weverson83\AddByLink\Api\LinkRepositoryInterface;
 
-class ReadHandler implements ExtensionInterface
+class CreateHandler implements ExtensionInterface
 {
 
     /**
-     * @inheritDoc
+     * @var \Weverson83\AddByLink\Api\LinkRepositoryInterface
+     */
+    protected $linkRepository;
+
+    public function __construct(LinkRepositoryInterface $linkRepository)
+    {
+        $this->linkRepository = $linkRepository;
+    }
+
+    /**
+     * Perform action on relation/extension attribute
+     *
+     * @param \Magento\Catalog\Api\Data\ProductInterface $entity
+     * @param array $arguments
+     * @return \Magento\Catalog\Api\Data\ProductInterface
      */
     public function execute($entity, $arguments = [])
     {
-        // TODO: Implement execute() method.
+        /** @var \Weverson83\AddByLink\Api\Data\LinkInterface[] $links */
+        $links = $entity->getExtensionAttributes()->getAddByLinkList() ?? [];
+
+        foreach ($links as $link) {
+            $link->setId(null);
+            $this->linkRepository->save($entity->getSku(), $link, !(bool)$entity->getStoreId());
+        }
     }
 }
--- Test/Integration/LinkManagementTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Weverson83\AddByLink\Test\Integration;
-
-class LinkManagementTest extends \PHPUnit\Framework\TestCase
-{
-
-}
